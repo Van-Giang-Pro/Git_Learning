bc6b7cbaa5badc3779ea6621627d696c60ab627a
Van Giang <vangiang260694@gmail.com>
2022-03-04 15:40:15 +0700

    Add Translator

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print('One')
-print('Two')
-print('Three')
+from translate import Translator
+translator = Translator(to_lang="de")
+print(translator.translate('One'))
+print(translator.translate('Two'))
+print(translator.translate('Three'))
